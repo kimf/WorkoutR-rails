8a6b9578e99946322c79f1b92cad30df235e0bda
Kim Fransman <kim.fransman@gmail.com>
2018-01-08 09:37:04 +0100

    use Rails.credentials for admin user/pass

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -7,8 +7,8 @@
 module Admin
   class ApplicationController < Administrate::ApplicationController
     http_basic_authenticate_with(
-      name: ENV.fetch("ADMIN_NAME"),
-      password: ENV.fetch("ADMIN_PASSWORD")
+      name: Rails.application.credentials.admin.user,
+      password: Rails.application.credentials.admin.password
     )
 
     # Override this value to specify the number of elements to display at a time
