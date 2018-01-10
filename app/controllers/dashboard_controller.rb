f2d9f3373b93fe4744d7105b0abe1b2a58022121
Kim Fransman <kim.fransman@gmail.com>
2018-01-10 23:57:17 +0100

    Show days worked out instead of weekly workouts

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,10 +3,20 @@ class DashboardController < ApplicationController
 
   def index
     today = Date.today
-    date_range = Date.parse('2017-04-27')..today
+    # first has to be monday for my super naive "count only days worked out not workouts"
+    date_range = Date.parse('2017-04-24')..today
 
     @measurements = Measurement.all.to_a
-    @consistency = Activity.group_by_week(:date, format: '%V', range: date_range).count
+    days = Activity.group_by_day(:date, format: '%Y-%m-%d', range: date_range).count
+
+    weeks = days.each_slice(7).to_a
+    @consistency = weeks.map do |week|
+      [
+        Date.parse(week[0][0]).strftime('%V'),
+        week.select{|d| d[1] > 0 }.size
+      ]
+    end
+
     @consistency_distance = Activity.group_by_month(:date, format: '%b', range: date_range).sum(:km)
 
     last_date = today.end_of_month + 2.months
